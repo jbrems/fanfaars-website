8c6a9dd81d05ae03435d04a8ef3b8b99949fac5b
Jonas Brems <jonas.brems@cm.be>
2019-10-30 20:02:31 +0100

    Header menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { firebaseConfig } from './firebase-config';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     AngularFireModule.initializeApp(firebaseConfig),
