320c9a16c08fb485687fe512622150a5fae6f134
Jonas Brems <jonasbrems@gmail.com>
2020-06-05 23:06:58 +0200

    Added announcement to main page

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -8,9 +8,11 @@ import { PhotoAlbumModule } from '../shared/photo-album/photo-album.module';
 import { HeroCarouselComponent } from './hero-carousel/hero-carousel.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import {AnnouncementComponent} from './announcement/announcement.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [HomePageComponent, HeroCarouselComponent],
+  declarations: [HomePageComponent, HeroCarouselComponent, AnnouncementComponent],
   imports: [
     ActivityModule,
     CommonModule,
@@ -19,6 +21,7 @@ import { HomePageRoutingModule } from './home-page-routing.module';
     PhotoAlbumModule,
     CarouselModule,
     HomePageRoutingModule,
+    RouterModule,
   ],
   exports: [HomePageComponent],
 })
