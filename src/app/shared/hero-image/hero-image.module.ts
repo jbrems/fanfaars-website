c67ad19dfaab9881c134a2454bcec52b961c1e3c
Jonas Brems <jonasbrems@gmail.com>
2022-07-24 19:22:15 +0200

    Update F4F page and small css fix

--- src/app/shared/hero-image/hero-image.module.ts
+++ src/app/shared/hero-image/hero-image.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeroImageComponent } from './hero-image.component';
 import { RouterModule } from '@angular/router';
@@ -6,6 +7,7 @@ import { RouterModule } from '@angular/router';
   declarations: [HeroImageComponent],
   exports: [HeroImageComponent],
   imports: [
+    CommonModule,
     RouterModule
   ]
 })
